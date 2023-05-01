027d687896a9f89cb1b9d54ccebc41933ac96ac4
xabibullayev <xabibullayevmm@gmail.com>
2023-05-01 16:41:09 +0500

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
 import "./app.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Navbar /> */}
+      <Navbar />
 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,38 @@
-"navbar.css";
+import "./navbar.css";
+import SearchOutlinedIcon from "@mui/icons-material/Search";
+import PersonOutlinedIcon from "@mui/icons-material/Person";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Islew keredasdasdasdask</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <img src="/images/download.png" alt="" />
+        </div>
+        <ul className="menu">
+          <li>
+            <Link to="/" className="link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="link">
+              About
+            </Link>
+          </li>
+          <li>Services</li>
+          <li>Contact</li>
+          <li>
+            <SearchOutlinedIcon />
+            <PersonOutlinedIcon />
+            <ShoppingCartOutlinedIcon />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
